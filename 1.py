b8e8a11d219e53032da240819ec0afa91a7c86e6
peteashemg <peteashem@gmail.com>
2026-04-23 09:23:50 +0000

    Удалил лишние файыл из репозитория

--- 1.py
+++
@@ -1,7 +0,0 @@
-s = input("s: ").strip()
-t = input("t: ").strip()
-
-if len(s) == len(t) and t in (s + s):
-    print("Результат: Yes")
-else:
-    print("Результат: No")
--- vimrc
+++
@@ -1,8 +0,0 @@
-syntax on
-filetype plugin indent on
-
-call plug#begin('~/.vim/plugged')
-plug 'elixir-editors/vim-elixir'
-
-call plug#end()
-
